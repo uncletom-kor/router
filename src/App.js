92977e725ae78bf757db8d0342e9642f9c907791
uncletom-kor <uncletom.kor@gmail.com>
2024-02-06 09:25:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Videos from './pages/Videos';
+import NotFound from './pages/NotFound';
+import Root from './pages/Root';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root />,
+        errorElement: <NotFound />,
+
+        
+        children: [
+            { index: true, element: <Home /> },
+            { path: '/videos', element: <Videos /> },
+        ],
+    },
+]);
+
+export default function App() {
+    return <RouterProvider router={router}></RouterProvider>;
 }
-
-export default App;
